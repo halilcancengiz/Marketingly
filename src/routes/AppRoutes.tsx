55dd7cf31babdf1205e5f2897212b3c784e1db67
Halil Can Cengiz <88160209+halilcancengiz@users.noreply.github.com>
2024-11-09 18:30:42 +0300

    updated

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import { HomeV1 } from "../pages/HomeV1";
 import { HomeV2 } from "../pages/HomeV2";
-import { StyleGuidePage } from "../pages/StyleGuidePage";
-import Test from "../pages/Test";
+// import { StyleGuidePage } from "../pages/StyleGuidePage";
+// import Test from "../pages/Test";
 import NotFound from "../pages/NotFound";
 import Thankyou from "../pages/Thankyou";
 import TermAndConditions from "../pages/TermAndConditions";
