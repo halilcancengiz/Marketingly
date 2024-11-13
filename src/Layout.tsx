0fcc0799c9883b8bbe445de4eaa1a689fe60b619
Halil Can Cengiz <88160209+halilcancengiz@users.noreply.github.com>
2024-11-13 17:48:49 +0300

    updated

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,11 +7,11 @@ import { AppRoutes } from './routes/AppRoutes';
 
 const Layout = () => {
   const location = useLocation();
-  const [contentVisible, setContentVisible] = useState(false);
+  const [contentVisible, setContentVisible] = useState(true); // Varsayılan olarak true ayarlandı
   const [animationStarted, setAnimationStarted] = useState(false);
   const [previousWidth, setPreviousWidth] = useState(window.innerWidth);
   const [previousPathname, setPreviousPathname] = useState(location.pathname);
-  const [animationKey, setAnimationKey] = useState(0); // animasyon anahtarını takip etmek için
+  const [animationKey, setAnimationKey] = useState(0);
 
   const breakpoints = [480, 768, 992];
 
@@ -21,7 +21,7 @@ const Layout = () => {
 
     setTimeout(() => {
       setAnimationStarted(true);
-      setAnimationKey(prev => prev + 1); // animasyon anahtarını güncelle
+      setAnimationKey((prev) => prev + 1);
     }, 100);
   };
 
@@ -50,13 +50,12 @@ const Layout = () => {
     const handleResize = () => {
       const currentWidth = window.innerWidth;
 
-      // Önceki genişlik ile şimdiki genişlik arasında breakpoint geçişi var mı kontrol et
-      const isPreviousCrossingBreakpoint = breakpoints.some(bp =>
-        (previousWidth < bp && currentWidth >= bp) ||
-        (previousWidth >= bp && currentWidth < bp)
+      const isPreviousCrossingBreakpoint = breakpoints.some(
+        (bp) =>
+          (previousWidth < bp && currentWidth >= bp) ||
+          (previousWidth >= bp && currentWidth < bp)
       );
 
-      // Eğer bir breakpoint'i geçtiyse animasyonu başlat
       if (isPreviousCrossingBreakpoint) {
         startAnimation();
       }
@@ -75,10 +74,10 @@ const Layout = () => {
     <div className="flex flex-col relative">
       {animationStarted && !contentVisible && (
         <motion.div
-          key={`fadeOut-${animationKey}`} // animasyon anahtarını key olarak kullan
+          key={`fadeOut-${animationKey}`}
           className="h-[4px] bg-primary absolute top-0 left-0"
-          initial={{ width: "100%" }}
-          animate={{ width: "0%" }}
+          initial={{ width: '100%' }}
+          animate={{ width: '0%' }}
           transition={{ duration: 0.001, delay: 0.5 }}
           style={{ zIndex: 10 }}
           onAnimationStart={() => setTimeout(() => setContentVisible(true), 1000)}
@@ -86,16 +85,16 @@ const Layout = () => {
       )}
 
       <motion.div
-        key={`fadeIn-${animationKey}`} // animasyon anahtarını key olarak kullan
+        key={`fadeIn-${animationKey}`}
         className="h-[4px] bg-primary absolute top-0 left-0"
-        initial={{ width: "0%" }}
-        animate={{ width: "100%" }}
+        initial={{ width: '0%' }}
+        animate={{ width: '100%' }}
         transition={{ duration: 1, delay: 0.5 }}
         style={{ zIndex: 10 }}
         onAnimationComplete={() => {
-          const element = document.querySelector(".bg-primary") as HTMLDivElement;
+          const element = document.querySelector('.bg-primary') as HTMLDivElement;
           if (element) {
-            element.style.opacity = "0";
+            element.style.opacity = '0';
           }
         }}
       />
